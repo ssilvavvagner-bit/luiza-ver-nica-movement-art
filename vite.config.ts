0b8996372f0792008173fb3d664e9a7d54c8ea52
ssilvavvagner-bit <ssilva.vvagner@gmail.com>
2026-03-20 19:56:33 -0300

    Setup GitHub Pages with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/luiza-ver-nica-movement-art/",
   server: {
     host: "::",
     port: 8080,
