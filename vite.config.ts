9730cbeea1da1561ab691f95853d8a0add871a94
ssilvavvagner-bit <ssilva.vvagner@gmail.com>
2026-03-20 23:22:01 -0300

    inserir dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/luiza-ver-nica-movement-art/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
